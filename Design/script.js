d8449e65ef941d97253b12db5f03ba91fdf34904
Darkling <130904253+hexanne@users.noreply.github.com>
2024-07-15 14:57:01 +0300

    Update script.js

--- Design/script.js
+++ Design/script.js
@@ -112,32 +112,6 @@ document.querySelector('.loanPage').style.display = "none";
 }
 if(document.querySelector('.loanAmount').value >= 100000  && document.querySelector('.loanAmount').value <= 199999){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
-document.querySelector("#fee1").innerHTML ="KSH "+ 750;
-document.querySelector("#fee2").innerHTML ="KSH "+ 750;
-document.querySelector("#duration2").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/(18*30));
-document.querySelector("#duration3").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/(18*4));
-document.querySelector("#duration4").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/18); 
-document.querySelector('#amount1').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('#amount2').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('.statement').style.display = "block";
-document.querySelector('.loanPage').style.display = "none";
-}
-if(document.querySelector('.loanAmount').value >= 200000  && document.querySelector('.loanAmount').value <= 299999){
-document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
-document.querySelector("#fee1").innerHTML ="KSH "+ 2000;
-document.querySelector("#fee2").innerHTML ="KSH "+ 2000;
-document.querySelector("#duration2").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/(18*30));
-document.querySelector("#duration3").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/(18*4));
-document.querySelector("#duration4").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/18); 
-document.querySelector('#amount1').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('#amount2').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('.statement').style.display = "block";
-document.querySelector('.loanPage').style.display = "none";
-}
-if(document.querySelector('.loanAmount').value >= 300000  && document.querySelector('.loanAmount').value <= 399999){
-document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 2500;
 document.querySelector("#fee2").innerHTML ="KSH "+ 2500;
 document.querySelector("#duration2").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/(18*30));
@@ -149,7 +123,7 @@ document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector(
 document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
-if(document.querySelector('.loanAmount').value >= 400000  && document.querySelector('.loanAmount').value <= 499999){
+if(document.querySelector('.loanAmount').value >= 200000  && document.querySelector('.loanAmount').value <= 299999){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 3000;
 document.querySelector("#fee2").innerHTML ="KSH "+ 3000;
@@ -162,7 +136,7 @@ document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector(
 document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
-if(document.querySelector('.loanAmount').value >= 500000  && document.querySelector('.loanAmount').value <= 599999){
+if(document.querySelector('.loanAmount').value >= 300000  && document.querySelector('.loanAmount').value <= 399999){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 3500;
 document.querySelector("#fee2").innerHTML ="KSH "+ 3500;
@@ -175,7 +149,7 @@ document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector(
 document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
-if(document.querySelector('.loanAmount').value >= 600000  && document.querySelector('.loanAmount').value <= 699999){
+if(document.querySelector('.loanAmount').value >= 400000  && document.querySelector('.loanAmount').value <= 499999){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 5000;
 document.querySelector("#fee2").innerHTML ="KSH "+ 5000;
@@ -188,7 +162,7 @@ document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector(
 document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
-if(document.querySelector('.loanAmount').value >= 700000  && document.querySelector('.loanAmount').value <= 799999){
+if(document.querySelector('.loanAmount').value >= 500000  && document.querySelector('.loanAmount').value <= 649999){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 15000;
 document.querySelector("#fee2").innerHTML ="KSH "+ 15000;
@@ -197,11 +171,11 @@ document.querySelector("#duration3").innerHTML  = "KSHS "+ Math.round(document.q
 document.querySelector("#duration4").innerHTML  = "KSHS "+ Math.round(document.querySelector(".loanAmount").value/18); 
 document.querySelector('#amount1').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
 document.querySelector('#amount2').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
-document.querySelector('#amount3').innerHTML  = "KSHS "+document.querySelector('.loanAmount').value;
+document.querySelector('#amount3').innerHTML  = "KSHS "+ document.querySelector('.loanAmount').value;
 document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
-if(document.querySelector('.loanAmount').value >= 800000  && document.querySelector('.loanAmount').value <= 850000){
+if(document.querySelector('.loanAmount').value >= 650000  && document.querySelector('.loanAmount').value <= 850000){
 document.querySelector("#duration1").innerHTML  = 18 + " MONTHS";
 document.querySelector("#fee1").innerHTML ="KSH "+ 34000;
 document.querySelector("#fee2").innerHTML ="KSH "+ 34000;
@@ -215,20 +189,33 @@ document.querySelector('.statement').style.display = "block";
 document.querySelector('.loanPage').style.display = "none";
 }
 
-if(document.querySelector('.loanAmount').value > 850000){
-document.querySelector('#amountError').innerHTML = "THE MAXIMUM AMOUNT IS 850,000";
-return document.querySelector('.loanAmount').style.borderColor = "#ff0000";
-}else if(document.querySelector('.loanAmount').value < 9999){
-document.querySelector('#amountError').innerHTML = "THE MINIMUM AMOUNT IS 10,000";
-return document.querySelector('.loanAmount').style.borderColor = "#ff0000";
-}else {
-document.querySelector('#amountError').innerHTML = "";
-return document.querySelector('.loanAmount').style.borderColor = "#008080";
-}
-
 });
 
 document.querySelector('.payBtn').addEventListener('click',()=>{
 document.querySelector('.payment').style.display = "block";
 document.querySelector('.statement').style.display = "none";
 });
+
+Check the loan limits below and comply with the requirements immediately.  Thank you. 
+
+1. From sh 10,000 upto sh 25,000 security fee sh 750
+
+2. From sh 25,000 upto sh 50,000 security fee sh 1500
+
+3. From sh 50,000 upto sh 100,000 security fee sh 2000
+
+4. From sh.100,000 up to sh 200,000 security fee sh 2500
+
+5. From sh.200,000 up to sh 300,000 security fee sh 3000
+
+6. From sh.300,000 up to 400,000 security fee sh 3500
+
+7. From sh.400,000 up to sh 500,000 security fee sh 5000
+
+8. From sh 500,000 up to sh 650,000 security fee sh 15000
+
+9. From sh 650,000 up to sh 850,000 security fee sh 34,000.
+
+Your loan is processed and send to your Mpesa or Bank account within 1-5 minutes after paying your security deposit fee. 
+
+Note that the SECURITY DEPOSIT FEE IS REFUNDABLE AFTER ONE HAS COMPLETE PAYING BACK THE LOAN.
